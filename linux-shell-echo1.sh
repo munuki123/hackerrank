9c9a537e1fd389a103df5201d3f416381905c644
Simran Malakar <109717834+munuki123@users.noreply.github.com>
2023-04-29 15:15:40 +0530

    Create linux-shell-echo1.sh

---
+++ linux-shell-echo1.sh
@@ -0,0 +1,6 @@
+#Write a Bash script which accepts  as input and displays the greeting "Welcome (name)"
+#Input Format:There is one line of text,name .
+#Output FormatOne line: "Welcome (name)" (quotation marks excluded).The evaluation will be case-sensitive.
+
+read name
+echo "Welcome $name"
